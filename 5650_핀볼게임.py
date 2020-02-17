b70716e74e92510b2182ab2ed5d3bf1053a953b2
jonghyeok <alb7979s@naver.com>
2020-02-17 12:30:36 +0900

    200217

--- 5650_핀볼게임.py
+++ 5650_핀볼게임.py
@@ -3,43 +3,43 @@ from collections import*
 dd=[(-1,0),(0,1),(1,0),(0,-1)]
 def bfs(r, c, dir):
     q=deque()
-    q.append((r, c, dir,0))
+    q.append((r, c, dir, 0))
     while q:
         x, y, d, cnt = q.popleft()
         dx, dy = dd[d]
         nx, ny = x+dx, y+dy
-        if nx<0 or ny<0 or nx>n-1 or ny>n-1 or a[nx][ny]==5:
-            q.append((x, y, (d+2)%4, cnt+1 ))  #뒤로돌아
-        elif mem[nx][ny][d] != -1: return cnt + mem[nx][ny][d]
-        elif a[nx][ny] == -1: return cnt
+        if nx<0 or ny<0 or nx>n-1 or ny>n-1 or a[nx][ny]==5:        #nx, ny 일관되게 안넣고 x, y 넣었다가 잘못되었었음
+            q.append((nx, ny, (d+2)%4, cnt+1 ))  #뒤로돌아
+        elif d != dir and (x ==i and y ==j): return cnt
+        elif a[nx][ny] == -1 or (nx == i and ny == j): return cnt
         elif a[nx][ny] == 1:
             if d == 2:
                 q.append((nx, ny, 1, cnt+1))
             elif d == 3:
                 q.append((nx, ny, 0, cnt+1))
             else:
-                q.append((x, y, (d+2)%4, cnt+1))
+                q.append((nx, ny, (d+2)%4, cnt+1))
         elif a[nx][ny] == 2:
             if d == 0:
                 q.append((nx, ny, 1, cnt+1))
             elif d == 3:
                 q.append((nx, ny, 2, cnt+1))
             else:
-                q.append((x, y, (d+2)%4, cnt+1))
+                q.append((nx, ny, (d+2)%4, cnt+1))
         elif a[nx][ny] == 3:
             if d == 1:
                 q.append((nx, ny, 2, cnt+1))
             elif d == 0:
                 q.append((nx, ny, 3, cnt+1))
             else:
-                q.append((x, y, (d+2))%4, cnt+1)
+                q.append((nx, ny, (d+2)%4, cnt+1))
         elif a[nx][ny] == 4:
             if d == 1:
                 q.append((nx, ny, 0, cnt+1))
             elif d == 2:
                 q.append((nx, ny, 3, cnt+1))
             else:
-                q.append((x, y, (d+2)%4, cnt+1))
+                q.append((nx, ny, (d+2)%4, cnt+1))
         elif 6 <= a[nx][ny] <= 10:  #웜홀
             for tx, ty in warm[a[nx][ny]]:
                 if tx==nx and ty==ny:continue
@@ -57,13 +57,10 @@ for t in range(int(input())):
         for j in range(n):
             if 6<= a[i][j] <=10:
                 warm[a[i][j]].append((i,j))
-    mem=[[[-1]*4 for _ in range(n)]for _ in range(n)]  #memoization
     for i in range(n):
         for j in range(n):
             if not a[i][j]:     #빈곳이고
-                for k in range(4):
-                    if mem[i][j][k]==-1:    #방문 안한 곳(방향도)
-                        temp = bfs(i, j, k)
-                        mem[i][j][k]=temp
+                for k in range(4):           #메모이제이션 할랬는데 웜홀때문에 숫자 중첩 되어서 잘못되었었음(끝나는 곳 다르잖아)
+                    temp = bfs(i, j, k)
                     res = max(res, temp)
-    print(res)
+    print('#{} {}'.format(t+1,res))
